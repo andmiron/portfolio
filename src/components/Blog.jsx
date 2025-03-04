01c50906c38e704f237fd9e9dfcec22d1e22ba6f
Andrii <miron6@ukr.net>
2025-03-04 13:52:31 +0200

    refactor: replace TextSkeleton with loading spinner and enhance Home component content

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,14 +1,13 @@
 import { useEffect, useState } from "react";
 import * as cheerio from "cheerio";
-import TextSkeleton from "./TextSkeleton.jsx";
 import BlogCard from "./BlogCard.jsx";
+import { LuLoaderCircle } from "react-icons/lu";
 
 export default function Blog() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(false);
     fetch(
       "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40sylneyshii"
     )
@@ -40,9 +39,11 @@ export default function Blog() {
     <div>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Blog</h1>
       <div className="container mx-auto">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 relative">
           {loading ? (
-            <TextSkeleton />
+            <div className="absolute inset-0 flex items-center justify-center h-100">
+              <LuLoaderCircle className="animate-spin h-12 w-12 text-gray-700" />
+            </div>
           ) : (
             articles.map((article) => (
               <BlogCard article={article} key={article.title} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,48 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Welcome</h1>
+      <h1 className="font-semibold text-2xl mb-4">Welcome</h1>
       <p className="mb-3">Hello and welcome on my website!</p>
       <p className="mb-3">
         I am a passionate Node.js developer, always eager to learn new things.
       </p>
       <p className="mb-3">
-        In my opinion, the only way to become a good software engineer is make a
-        huge number of mistakes and then solve them. So I am out here to learn
-        new things, and continue on the path to becoming a software engineer.
-      </p>
-      <p className="mb-3">
         Here, you’ll find personal information, examples of my projects, and a
         blog where I share some thoughts on development and tech in general.
       </p>
-      <p className="mb-3">
+      <p className="mb-6">
         I’m currently seeking exciting opportunities to collaborate and grow,
         and I’d love for you to explore my work!
+      </p>
+
+      <h2 className="text-xl font-semibold mb-4">Little note about me</h2>
+      <p className="mb-3">
+        My life was dedicated to professional sports. I have been playing
+        basketball since I was 7 years old. What started as a childhood hobby
+        grew into a profession that gave me the opportunity to explore the
+        world, meet incredible people, and undoubtedly shaped me into a strong
+        person both mentally and physically.
+      </p>
+      <p className="mb-3">
+        However, I knew that such great things aren’t meant to last forever, so
+        I always made sure to prioritize my education throughout those years.
+        Having finished school successfully, I wanted to pursue a degree in
+        computer science, but my parents chose another field, believing it was
+        not a &apos;serious&apos; profession at the time. Despite this, the
+        desire to become a software engineer never left me.
+      </p>
+      <p className="mb-3">
+        Now, as my career as a professional athlete comes to an end, I am
+        determined to become a professional software engineer. It is not an easy
+        walk these days, especially when many fear that AI will replace
+        developers in the near future.
+      </p>
+      <p className="mb-3">
+        But I firmly believe that a good software engineer cannot be replaced by
+        a machine, no matter how powerful it may be.
+      </p>
+      <p className="mb-3 font-semibold">
+        After all, software engineering is much more than just writing code...
       </p>
     </div>
   );
